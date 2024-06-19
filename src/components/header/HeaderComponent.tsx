12cae17e282f2edb069e430bf98b1782d9631f0f
Roko <rokoponjarac2@gmail.com>
2024-06-19 12:01:32 +0200

    add functionality on menu

--- src/components/header/HeaderComponent.tsx
+++ src/components/header/HeaderComponent.tsx
@@ -1,26 +1,36 @@
+import { useState } from "react";
 import styles from "./HeaderComponent.module.scss";
+import MenuComponent from "./menu/MenuComponent";
 
 const HeaderComponent = () => {
+  const [isMenuActive, setIsMenuActive] = useState<boolean>(false);
+
   return (
-    <header className={styles.header}>
-      <div className={styles.header__container}>
-        <div className={styles.header__main}>
-          <a className={styles.header__main_logo}>
-            <img
-              src={"/logo-desktop.svg"}
-              alt="logo"
-              className={styles.header__main__logo__desktop}
-            />
-            <img
-              src={"/logo-mobile.svg"}
-              alt="logo"
-              className={styles.header__main__logo__mobile}
-            />
-          </a>
-          <div className={styles.header__main__links}></div>
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__container}>
+          <div className={styles.header__main}>
+            <a className={styles.header__main_logo}>
+              <img
+                src={"/logo-desktop.svg"}
+                alt="logo"
+                className={styles.header__main__logo__desktop}
+              />
+              <img
+                src={"/logo-mobile.svg"}
+                alt="logo"
+                className={styles.header__main__logo__mobile}
+              />
+            </a>
+            <div className={styles.header__main__links}></div>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <MenuComponent
+        isActive={isMenuActive}
+        closeModal={() => setIsMenuActive(false)}
+      />
+    </>
   );
 };
 
