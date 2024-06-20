76e38470a66cf60eb0e81c97e1f546ed21a6d858
Roko <rokoponjarac2@gmail.com>
2024-06-20 18:19:54 +0200

    make authentication form

--- src/pages/authentication-page/components/authentication-form/AuthenticationFormComponent.tsx
+++ src/pages/authentication-page/components/authentication-form/AuthenticationFormComponent.tsx
@@ -1,7 +1,57 @@
+import { z } from "zod";
 import styles from "./AuthenticationFormComponent.module.scss";
+import { authenticationSchema } from "../../../../types/schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type TAuthenticationSchema = z.infer<typeof authenticationSchema>;
 
 const AuthenticationFormComponent = () => {
-  return <div>AuthenticationFormComponent</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<TAuthenticationSchema>({
+    resolver: zodResolver(authenticationSchema),
+  });
+
+  const onSubmit = async (data: TAuthenticationSchema) => {
+    console.log(data);
+
+    reset();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={styles.authentication_form}
+    >
+      <label className={styles.authentication_form__label}>
+        <input
+          type="text"
+          {...register("username")}
+          placeholder="Unesi korinsičko ime"
+          autoComplete={"on"}
+        />
+        {errors.username && (
+          <p className="form_error">{`${errors.username.message}`}</p>
+        )}
+      </label>
+      <label className={styles.authentication_form__label}>
+        <input
+          type="password"
+          {...register("password")}
+          placeholder="Unesi šifru"
+          autoComplete={"on"}
+        />
+        {errors.password && (
+          <p className="form_error">{`${errors.password.message}`}</p>
+        )}
+      </label>
+      <button onClick={handleSubmit(onSubmit)}>Prijavi se</button>
+    </form>
+  );
 };
 
 export default AuthenticationFormComponent;
