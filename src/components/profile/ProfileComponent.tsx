8be6b2b216c300ea461383b152e158fb5db17fc4
Roko <rokoponjarac2@gmail.com>
2024-06-23 16:40:10 +0200

    make profile button with functionality

--- src/components/profile/ProfileComponent.tsx
+++ src/components/profile/ProfileComponent.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./ProfileComponent.module.scss";
 import { authManager } from "../../util/useAuthContext";
 import ButtonComponent from "../button/ButtonComponent";
@@ -9,10 +9,30 @@ const ProfileComponent = () => {
 
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const modalRef = useRef<HTMLDivElement | null>(null);
+  const modalButton = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(e.target as Node) &&
+        modalButton.current &&
+        !modalButton.current.contains(e.target as Node)
+      ) {
+        setIsModalOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.addEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
 
   return (
     <>
-      <div className={styles.profile}>
+      <div className={styles.profile} ref={modalButton}>
         <div
           className={styles.profile__button}
           onClick={() => setIsModalOpen(!isModalOpen)}
