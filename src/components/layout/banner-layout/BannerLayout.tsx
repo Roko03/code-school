c960eb20f7ef1de0d0b95654058267362618453f
Roko <rokoponjarac2@gmail.com>
2024-06-24 19:16:35 +0200

    refactor banner layout with variant

--- src/components/layout/banner-layout/BannerLayout.tsx
+++ src/components/layout/banner-layout/BannerLayout.tsx
@@ -3,18 +3,25 @@ import ContainerLayout from "../../container/ContainerLayout";
 
 interface BannerLayoutProps {
   hasBanner: boolean;
+  variant?: "main" | "secondary";
   children: React.ReactNode;
   bannerTitle: string;
 }
 
 const BannerLayout: React.FC<BannerLayoutProps> = ({
   hasBanner,
+  variant,
   children,
   bannerTitle,
 }) => {
   return (
     <>
-      {hasBanner && <BannerComponent variant={"main"} title={bannerTitle} />}
+      {hasBanner && (
+        <BannerComponent
+          variant={variant ? variant : "main"}
+          title={bannerTitle}
+        />
+      )}
       <ContainerLayout>{children}</ContainerLayout>
     </>
   );
--- src/routes/student/HomePage.tsx
+++ src/routes/student/HomePage.tsx
@@ -2,7 +2,11 @@ import BannerLayout from "../../components/layout/banner-layout/BannerLayout";
 
 const HomePage = () => {
   return (
-    <BannerLayout hasBanner={true} bannerTitle={"Naslovna"}>
+    <BannerLayout
+      hasBanner={true}
+      variant={"secondary"}
+      bannerTitle={"Naslovna"}
+    >
       <div>HomePage</div>
     </BannerLayout>
   );
