b31142c7ad7f27f411c40f895522e2c59257cced
Roko <rokoponjarac2@gmail.com>
2024-06-27 12:31:38 +0200

    make admin user page

---
+++ src/pages/admin-user-page/AdminUserPageSection.tsx
@@ -0,0 +1,7 @@
+import styles from "./AdminUserPageSection.module.scss";
+
+const AdminUserPageSection = () => {
+  return <section>AdminUserPageSection</section>;
+};
+
+export default AdminUserPageSection;
