a9f7e48bdf519fa88c51877af0327644477bf65e
Roko <rokoponjarac2@gmail.com>
2024-06-27 13:09:32 +0200

    make selector by link

--- src/pages/admin-user-page/AdminUserPageSection.tsx
+++ src/pages/admin-user-page/AdminUserPageSection.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import styles from "./AdminUserPageSection.module.scss";
 import AdminUserRoleSelector from "./components/admin-user-role-selector/AdminUserRoleSelector";
+import { useSearchParams } from "react-router-dom";
 
 const AdminUserPageSection = () => {
+  const [searchParams] = useSearchParams();
+  const query: "-" | "adm" | "prof" | "stu" =
+    searchParams.get("role") !== null
+      ? (searchParams.get("role") as "adm" | "prof" | "stu")
+      : "-";
   const [roleSelected, setRoleSelected] = useState<
     "-" | "adm" | "prof" | "stu"
-  >("-");
+  >(query);
 
   return (
     <section className={styles.admin_users_section}>
