73dd394aa9410a1ff5c2dc198aa7bad88c1d6805
Roko <rokoponjarac2@gmail.com>
2024-06-28 13:02:01 +0200

    refactor admin user page section

--- src/pages/admin-user-page/AdminUserPageSection.tsx
+++ src/pages/admin-user-page/AdminUserPageSection.tsx
@@ -1,18 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./AdminUserPageSection.module.scss";
 import AdminUserRoleSelector from "./components/admin-user-role-selector/AdminUserRoleSelector";
 import { useSearchParams } from "react-router-dom";
 import AdminUserListComponent from "./components/admin-user-list/AdminUserListComponent";
+import getAllUser from "../../lib/user/getAllUser";
+import filterUser from "../../lib/user/filterUser";
 
 const AdminUserPageSection = () => {
   const [searchParams] = useSearchParams();
+
   const query: "-" | "adm" | "prof" | "stu" =
     searchParams.get("role") !== null
       ? (searchParams.get("role") as "adm" | "prof" | "stu")
       : "-";
+
   const [roleSelected, setRoleSelected] = useState<
     "-" | "adm" | "prof" | "stu"
   >(query);
+
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [modalVariant, setModalVariant] = useState<
+    null | "add" | "edit" | "delete"
+  >(null);
+
+  const [userList, setUserList] = useState<null | UserType[]>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const fetchUsers = async () => {
+    setIsLoading(true);
+
+    let response;
+
+    if (query == "-") {
+      response = await getAllUser();
+    } else {
+      response = await filterUser(query);
+    }
+
+    setUserList(response);
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [query]);
 
   return (
     <section className={styles.admin_users_section}>
@@ -22,7 +54,12 @@ const AdminUserPageSection = () => {
           setRoleSelected(value)
         }
       />
-      <AdminUserListComponent type={query} />
+      <AdminUserListComponent
+        type={query}
+        userList={userList}
+        isLoading={isLoading}
+      />
+      {isModalOpen && <div>EJ</div>}
     </section>
   );
 };
--- src/pages/admin-user-page/components/admin-user-list/AdminUserListComponent.tsx
+++ src/pages/admin-user-page/components/admin-user-list/AdminUserListComponent.tsx
@@ -1,62 +1,18 @@
-import { useEffect, useState } from "react";
 import AdminUserTableComponent from "./components/admin-user-table/AdminUserTableComponent";
 import AdminUserListMobileComponent from "./components/admin-user-list-mobile/AdminUserListMobileComponent";
 import CircularProgressComponent from "../../../../components/circular-progress/CircularProgressComponent";
-import getAllUser from "../../../../lib/user/getAllUser";
-import filterUser from "../../../../lib/user/filterUser";
 
 interface AdminUserListComponentProps {
   type: "-" | "adm" | "prof" | "stu";
+  userList: null | UserType[];
+  isLoading: boolean;
 }
 
 const AdminUserListComponent: React.FC<AdminUserListComponentProps> = ({
   type,
+  userList,
+  isLoading,
 }) => {
-  const [userList, setUserList] = useState<null | UserType[]>([
-    {
-      id: 1,
-      username: "User1",
-      email: "user1@gmaiil.com",
-      bio: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed libero ab praesentium molestias quaerat, dolore autem fuga necessitatibus minus. Voluptatum placeat enim cum sunt ad nihil rerum impedit similique? Odit illum, natus praesentium dolores asperiores tempore voluptatibus, quaerat rem modi quia maxime ab quo dolore reprehenderit. Facere officiis dolore quis!",
-      role: "prof",
-    },
-    {
-      id: 2,
-      username: "User2",
-      email: "user2@gmaiil.com",
-      bio: null,
-      role: "adm",
-    },
-    {
-      id: 3,
-      username: "User3",
-      email: "user3@gmaiil.com",
-      bio: null,
-      role: "stu",
-    },
-  ]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const fetchUsers = async () => {
-    setIsLoading(true);
-
-    let response;
-
-    if (type == "-") {
-      response = await getAllUser();
-    } else {
-      response = await filterUser(type);
-    }
-
-    setUserList(response);
-
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, [type]);
-
   if (isLoading) {
     return <CircularProgressComponent />;
   }
