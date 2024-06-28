9b9b1a67970ed94518fee8b8eceb13646426d1fc
Roko <rokoponjarac2@gmail.com>
2024-06-28 13:49:39 +0200

    make admi user form schema

---
+++ src/pages/admin-user-page/components/admin-user-form/AdminUserFormComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminUserFormComponent = () => {
+  return <div>AdminUserFormComponent</div>;
+};
+
+export default AdminUserFormComponent;
--- src/types/schema.ts
+++ src/types/schema.ts
@@ -10,3 +10,14 @@ export const loginSchema = z.object({
     email: z.string().min(1, { message: "Unesite email" })
         .refine((text) => patterns.email.test(text), { message: "Unesite valjani email" })
 })
+
+export const adminUserFormSchema = z.object({
+    username: z.string().min(1, { message: "Unesite korisničko ime" }),
+    email: z.string().min(1, { message: "Unesite email" })
+        .refine((text) => patterns.email.test(text), { message: "Unesite valjani email" }),
+    password: z.string().min(6, { message: 'Šifra treba imati minimalno 6 znakova' }),
+    bio: z.string().min(10, { message: "Unesite biografiju za korisnika" }),
+    role: z.enum(["adm", "stu", "prof"], {
+        errorMap: () => ({ message: "Odaberi ulogu" })
+    })
+})
