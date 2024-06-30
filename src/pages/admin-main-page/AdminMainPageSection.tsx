95acdb002c772899821fe4e9c25dd440032ebc7f
Roko <rokoponjarac2@gmail.com>
2024-06-30 12:49:31 +0200

    add title to main page

--- src/pages/admin-main-page/AdminMainPageSection.tsx
+++ src/pages/admin-main-page/AdminMainPageSection.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { authManager } from "../../util/useAuthContext";
+import styles from "./AdminMainPageSection.module.scss";
 
 const AdminMainPageSection = () => {
-  return <section></section>;
+  const auth = authManager();
+  return (
+    <section className={styles.admin_main_section}>
+      <h1>Dobrodošao {auth.user?.username}, vaša uloga je admin</h1>
+    </section>
+  );
 };
 
 export default AdminMainPageSection;
