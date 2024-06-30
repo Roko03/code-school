2967542e1e0d94a6a8a2032ec3cf9eab9c3921d1
Roko <rokoponjarac2@gmail.com>
2024-06-30 16:39:35 +0200

    add snack bar

--- src/pages/student-workshop-page/StudentWorkshopPageSection.tsx
+++ src/pages/student-workshop-page/StudentWorkshopPageSection.tsx
@@ -4,11 +4,35 @@ import StudentWorkshopListComponent from "./components/student-workshop-list/Stu
 import getWorkshopByUser from "../../lib/workshop/getWorkshopByUser";
 import { authManager } from "../../util/useAuthContext";
 import appliedToWorkshop from "../../lib/workshop/appliedToWorkshop";
+import SnackBarComponent from "../../components/snack-bar/SnackBarComponent";
 
 const StudentWorkshopPageSection = () => {
   const auth = authManager();
   const [workshopList, setWorkshopList] = useState<null | WorkshopType[]>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [snackBar, setSnackBar] = useState<SnackBarType>({
+    isOpen: false,
+    message: null,
+    type: null,
+  });
+
+  const closeSnackBarComponent = () => {
+    setSnackBar((prev) => {
+      return {
+        ...prev,
+        isOpen: false,
+      };
+    });
+  };
+
+  const openSnackBar = (type: "error" | "success", message: string) => {
+    setSnackBar({
+      isOpen: true,
+      message: message,
+      type: type,
+    });
+  };
 
   const fetchWorkshops = async () => {
     setIsLoading(true);
@@ -26,9 +50,11 @@ const StudentWorkshopPageSection = () => {
     const response = await appliedToWorkshop(workshop_id, auth.user.id);
 
     if (!response.success) {
+      openSnackBar("error", response.message);
       return;
     }
 
+    openSnackBar("success", response.message);
     fetchWorkshops();
   };
 
@@ -37,13 +63,21 @@ const StudentWorkshopPageSection = () => {
   }, []);
 
   return (
-    <section className={styles.student_workshop_section}>
-      <StudentWorkshopListComponent
-        workshopList={workshopList}
-        isLoading={isLoading}
-        workshopFunction={(id: number) => appliedToWorkshopFunction(id)}
+    <>
+      <section className={styles.student_workshop_section}>
+        <StudentWorkshopListComponent
+          workshopList={workshopList}
+          isLoading={isLoading}
+          workshopFunction={(id: number) => appliedToWorkshopFunction(id)}
+        />
+      </section>
+      <SnackBarComponent
+        variant={snackBar.type}
+        message={snackBar.message}
+        isVisible={snackBar.isOpen}
+        closeSnackBar={() => closeSnackBarComponent()}
       />
-    </section>
+    </>
   );
 };
 
