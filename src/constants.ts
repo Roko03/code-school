d83905ea2ce107615f1b27ae632352824dded70b
Roko <rokoponjarac2@gmail.com>
2024-06-30 12:35:41 +0200

    refactor errors

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,9 @@ export const patterns = {
     email: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
 };
 
-export const LEVELS = { 'jun': "Junior", 'mid': 'Mid', 'sen': 'Senior' }
+export const LEVELS: { [key: string]: any } = { 'jun': "Junior", 'mid': 'Mid', 'sen': 'Senior' }
 
-export const SUBJECTS = { 'rjs': "React.JS", 'ex': 'Express', 'njs': 'Next.JS' }
+export const SUBJECTS: { [key: string]: any } = { 'rjs': "React.JS", 'ex': 'Express', 'njs': 'Next.JS' }
 
 export const ACCESS_TOKEN = "access";
 export const REFRESH_TOKEN = "refresh"
