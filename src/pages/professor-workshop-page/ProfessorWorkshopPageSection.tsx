a462c2697981b13d32e901b7d7eae316457eaa88
Roko <rokoponjarac2@gmail.com>
2024-06-30 14:11:57 +0200

    make get professor workshop and refactor workshop

--- src/pages/professor-workshop-page/ProfessorWorkshopPageSection.tsx
+++ src/pages/professor-workshop-page/ProfessorWorkshopPageSection.tsx
@@ -5,30 +5,16 @@ import ProfessorWorkshopListComponent from "./components/professor-workshop-list
 const ProfessorWorkshopPageSection = () => {
   const [workshopList, setWorkshopList] = useState<
     ProfessorWorkshopType[] | null
-  >([
-    {
-      id: 7,
-      numb_of_users: 0,
-      name: "Radionica 1",
-      time: "2024-06-21T07:00:00Z",
-      info: "Radionica 1 opis",
-      level: "jun",
-      subject: "rjs",
-    },
-    {
-      id: 10,
-      numb_of_users: 0,
-      name: "Radionica 2",
-      time: "2024-06-13T21:11:00Z",
-      info: "afasgag",
-      level: "jun",
-      subject: "rjs",
-    },
-  ]);
+  >(null);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   return (
     <section className={styles.professor_workshop_section}>
-      <ProfessorWorkshopListComponent workshopList={workshopList} />
+      <ProfessorWorkshopListComponent
+        workshopList={workshopList}
+        isLoading={isLoading}
+      />
     </section>
   );
 };
