3b12b6956fa35606389f873c07bc01650ba948bb
Roko <rokoponjarac2@gmail.com>
2024-06-30 13:27:44 +0200

    make professor workshop section

---
+++ src/pages/professor-workshop-page/ProfessorWorkshopPageSection.tsx
@@ -0,0 +1,7 @@
+import styles from "./ProfessorWorkshopPageSection.module.scss";
+
+const ProfessorWorkshopPageSection = () => {
+  return <div>ProfessorWorkshopPageSection</div>;
+};
+
+export default ProfessorWorkshopPageSection;
