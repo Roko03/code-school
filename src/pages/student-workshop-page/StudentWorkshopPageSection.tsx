60d392611670b024ca662aaf1f93f670db5f5b44
Roko <rokoponjarac2@gmail.com>
2024-06-30 15:33:25 +0200

    make student workshop section

---
+++ src/pages/student-workshop-page/StudentWorkshopPageSection.tsx
@@ -0,0 +1,11 @@
+import styles from "./StudentWorkshopPageSection.module.scss";
+
+const StudentWorkshopPageSection = () => {
+  return (
+    <section className={styles.student_workshop_section}>
+      StudentWorkshopPageSection
+    </section>
+  );
+};
+
+export default StudentWorkshopPageSection;
