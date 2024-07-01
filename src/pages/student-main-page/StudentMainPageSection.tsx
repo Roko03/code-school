cc45fbc4e9e0d6f2e9ee9e83c77ab7abd9b629b1
Roko <rokoponjarac2@gmail.com>
2024-07-01 11:03:43 +0200

    get student workshop

--- src/pages/student-main-page/StudentMainPageSection.tsx
+++ src/pages/student-main-page/StudentMainPageSection.tsx
@@ -5,50 +5,7 @@ import StudentMainPageWorkshopList from "./components/student-workshop-list/Stud
 const StudentMainPageSection = () => {
   const [workshopList, setWorkshopList] = useState<
     StudentWorkshopLoginType[] | null
-  >([
-    {
-      id: 15,
-      workshop: {
-        id: 10,
-        user: {
-          id: 2,
-          username: "Jakov",
-          email: "jakov@gmail.com",
-          bio: "Jakov biografija1",
-          role: "prof",
-        },
-        numb_of_users: 1,
-        name: "Radionica 2",
-        time: "2024-06-13T21:11:00Z",
-        info: "afasgag",
-        level: "jun",
-        subject: "rjs",
-        user_id: 2,
-      },
-      workshop_id: 10,
-    },
-    {
-      id: 17,
-      workshop: {
-        id: 7,
-        user: {
-          id: 2,
-          username: "Jakov",
-          email: "jakov@gmail.com",
-          bio: "Jakov biografija1",
-          role: "prof",
-        },
-        numb_of_users: 1,
-        name: "Radionica 1",
-        time: "2024-06-21T07:00:00Z",
-        info: "Radionica 1 opis",
-        level: "jun",
-        subject: "rjs",
-        user_id: 2,
-      },
-      workshop_id: 7,
-    },
-  ]);
+  >(null);
 
   return (
     <section className={styles.student_main_section}>
